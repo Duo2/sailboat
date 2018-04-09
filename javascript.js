e97dc2fe984ec29683f7c75d3cfaf48a2527d9a3
Maren Sjøtveit <maren891@gmail.com>
2018-04-09 11:24:48 +0200

    Add main page

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(".hamburger").click(function() {
+
+	$(this).toggleClass("is-active");
+	$(".mobile_menu").toggle();
+
+});
+
+
+
+$(".hamburger").click(function() {
+  $(".mobile_menu" ).toggleClass( "open");
+});
